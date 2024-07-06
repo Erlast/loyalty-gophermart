09c180803ff50154bea438447e283739285f4b27
mmskazak <mmskazak@gmail.com>
2024-07-06 21:58:26 +0300

    update

--- internal/gofermart/handlers/load_orders.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-import (
-	"gofermart/pkg/zaplog"
-)
-
-func LoadOrders() {
-	zaplog.Logger.Info("load orders")
-}
--- internal/gofermart/migrations/gofermart/002_create_orders_table.sql
+++ internal/gofermart/migrations/gofermart/002_create_orders_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE orders (
+ CREATE TABLE orders (
                         id SERIAL PRIMARY KEY,
                         user_id INTEGER NOT NULL REFERENCES users(id),
                         number VARCHAR(255) NOT NULL UNIQUE,
