3fdc70e776cc0ed557b3a15dd66bbdb5ebb3cd1d
mmskazak <mmskazak@gmail.com>
2024-07-17 18:07:10 +0300

    upd

--- pkg/zaplog/logger.go
+++ pkg/zaplog/logger.go
@@ -6,11 +6,14 @@ import (
 	"go.uber.org/zap"
 )
 
+var Logger *zap.SugaredLogger
+
 // InitLogger создает и возвращает новый экземпляр zap SugaredLogger для разработки.
 func InitLogger() *zap.SugaredLogger {
 	zapLogger, err := zap.NewDevelopment()
 	if err != nil {
 		log.Fatalf("Error creating zap logger: %v", err)
 	}
+	Logger = zapLogger.Sugar()
 	return zapLogger.Sugar()
 }
