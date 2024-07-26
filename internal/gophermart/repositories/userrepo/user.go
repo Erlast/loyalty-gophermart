44e940ce06a8b37129ac1f7646dc844347366278
Jane A <bunny9@bk.ru>
2024-07-26 15:41:40 +0300

    common

--- internal/gophermart/repositories/userrepo/user.go
+++ internal/gophermart/repositories/userrepo/user.go
@@ -71,18 +71,23 @@ func (s *UserStorage) CreateUserTx(ctx context.Context, tx pgx.Tx, user *models.
 
 func (s *UserStorage) CreateUser(ctx context.Context, user *models.User) error {
 	// Проверяем наличие таблицы перед вставкой
-	_, err := tableExists(ctx, s.db, "users")
+	exist, err := tableExists(ctx, s.db, "users")
 	if err != nil {
 		return fmt.Errorf("error checking if table exists: %w", err)
 	}
+	if !exist {
+		return fmt.Errorf("table users doesn't exists ")
+	} else {
+		return fmt.Errorf("tablle exists")
+	}
 
 	// Запрос на вставку данных пользователя в таблицу
-	query := `INSERT INTO users (login, password, created_at, updated_at) VALUES ($1, $2, NOW(), NOW()) RETURNING id`
-	err = s.db.QueryRow(ctx, query, user.Login, user.Password).Scan(&user.ID)
-	if err != nil {
-		return fmt.Errorf("could not create user: %w", err)
-	}
-	return nil
+	//query := `INSERT INTO users (login, password, created_at, updated_at) VALUES ($1, $2, NOW(), NOW()) RETURNING id`
+	//err = s.db.QueryRow(ctx, query, user.Login, user.Password).Scan(&user.ID)
+	//if err != nil {
+	//	return fmt.Errorf("could not create user: %w", err)
+	//}
+	//return nil
 }
 
 func (s *UserStorage) GetUserByLogin(ctx context.Context, login string) (*models.User, error) {
